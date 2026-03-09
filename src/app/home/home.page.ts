6e942c0dd2bab7c82d39d0dedb1bd743bb648799
Han Naung Soe <hannaungsoe2@gmailcom>
2026-03-09 13:26:19 +0630

    feat: enhance home page layout and functionality with device information display and online status

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RefresherCustomEvent } from '@ionic/angular';
 import { AppInitService } from '../services/app-init.service';
 import { DeviceService } from '../services/device';
@@ -10,21 +10,31 @@ import { GenexusService } from '../services/genexus';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
   deviceInfo: any;
+  deviceId: any
+  lastCheckedAt = new Date();
+
   constructor(private readonly appInitService: AppInitService,
     private readonly deviceService: DeviceService,
     private readonly genexusService: GenexusService
   ) { }
 
   async ngOnInit(): Promise<void> {
-    await this.appInitService.initialize({ openWebsite: false });
     this.deviceInfo = await this.deviceService.getDeviceInfo();
+    this.deviceId = await this.deviceService.getDeviceId();
+    this.lastCheckedAt = new Date();
+    console.log('Device Info:', this.deviceInfo);
+  }
 
+  get isOnline(): boolean {
+    return navigator.onLine;
   }
 
   async reload(): Promise<void> {
+
     await this.appInitService.reloadWebsite();
+    this.lastCheckedAt = new Date();
   }
 
   async handleRefresh(event: RefresherCustomEvent): Promise<void> {
